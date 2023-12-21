f1bb8afc8c0eb327e8740d68d96744fe9bac15b2
youneslucy <younesyouness212u@gmail.com>
2023-12-22 00:39:32 +0100

    Add microservice_client_voiture.zip

---
+++ cont2/cont2/src/test/java/com/example/cont2/Cont2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cont2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cont2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
